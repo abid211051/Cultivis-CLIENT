9ba19e2f491d948d55d5274a129f478f64b41b47
abid211051 <apranto41@gmail.com>
2025-02-20 19:29:31 +0600

    aa

--- src/components/hometab/weather/homeWeather.jsx
+++ src/components/hometab/weather/homeWeather.jsx
@@ -11,6 +11,8 @@ const play = Play({
 export default async function HomeWeather() {
   // const currentweather = await currentWeather();
   // const hourlyweather = await hourlyWeather();
+  console.log("Little bit change");
+
   return (
     <div
       className={`${play.className} text-white bg-[url('/field2-min.png')] bg-cover bg-center lg:row-start-1 lg:row-span-1 lg:col-start-1 lg:col-span-6 rounded-xl  flex flex-col justify-between p-2 lg:m-0 mb-5`}
